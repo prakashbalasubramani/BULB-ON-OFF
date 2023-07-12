317a83c27f01098157b37fae88094edb8529003c
Prakash <138974999+prakashbalasubramani@users.noreply.github.com>
2023-07-11 22:27:50 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var on_btn = document.getElementById("on");
+var on_btn = document.getElementById("off");
+var img = document.getElementById("bulb");
+function bulb_on() {
+    img.src="./bulb on.png";
+
+}
+function bulb_off() {
+    img.src="./bulb off.png";
+    
+}
